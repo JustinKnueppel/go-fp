23bc6ec298570b67fe7926cbedf84c47a18353ba
Justin Knueppel <32350916+JustinKnueppel@users.noreply.github.com>
2022-09-06 18:32:37 -0400

    Add Compose function (#1)
    * Implement compose
    
    * Add nested test
    
    * Add docs

---
+++ compose.go
@@ -0,0 +1,57 @@
+package fp
+
+// Compose2 returns a right-to-left composition of 2 functions.
+func Compose2[A, B, C any](bc func(B) C, ab func(A) B) func(A) C {
+	return func(a A) C {
+		return bc(ab(a))
+	}
+}
+
+// Compose3 returns a right-to-left composition of 3 functions.
+func Compose3[A, B, C, D any](cd func(C) D, bc func(B) C, ab func(A) B) func(A) D {
+	return func(a A) D {
+		return cd(bc(ab(a)))
+	}
+}
+
+// Compose4 returns a right-to-left composition of 4 functions.
+func Compose4[A, B, C, D, E any](de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) E {
+	return func(a A) E {
+		return de(cd(bc(ab(a))))
+	}
+}
+
+// Compose5 returns a right-to-left composition of 5 functions.
+func Compose5[A, B, C, D, E, F any](ef func(E) F, de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) F {
+	return func(a A) F {
+		return ef(de(cd(bc(ab(a)))))
+	}
+}
+
+// Compose6 returns a right-to-left composition of 6 functions.
+func Compose6[A, B, C, D, E, F, G any](fg func(F) G, ef func(E) F, de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) G {
+	return func(a A) G {
+		return fg(ef(de(cd(bc(ab(a))))))
+	}
+}
+
+// Compose7 returns a right-to-left composition of 7 functions.
+func Compose7[A, B, C, D, E, F, G, H any](gh func(G) H, fg func(F) G, ef func(E) F, de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) H {
+	return func(a A) H {
+		return gh(fg(ef(de(cd(bc(ab(a)))))))
+	}
+}
+
+// Compose8 returns a right-to-left composition of 8 functions.
+func Compose8[A, B, C, D, E, F, G, H, I any](hi func(H) I, gh func(G) H, fg func(F) G, ef func(E) F, de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) I {
+	return func(a A) I {
+		return hi(gh(fg(ef(de(cd(bc(ab(a))))))))
+	}
+}
+
+// Compose9 returns a right-to-left composition of 9 functions.
+func Compose9[A, B, C, D, E, F, G, H, I, J any](ij func(I) J, hi func(H) I, gh func(G) H, fg func(F) G, ef func(E) F, de func(D) E, cd func(C) D, bc func(B) C, ab func(A) B) func(A) J {
+	return func(a A) J {
+		return ij(hi(gh(fg(ef(de(cd(bc(ab(a)))))))))
+	}
+}
---
+++ compose_test.go
@@ -0,0 +1,94 @@
+package fp_test
+
+import (
+	"testing"
+
+	"github.com/JustinKnueppel/go-fp"
+)
+
+func add1(x int) int {
+	return x + 1
+}
+
+func double(x int) int {
+	return x * 2
+}
+
+func strlen(s string) int {
+	return len(s)
+}
+
+func TestCompose2(t *testing.T) {
+	t.Run("composes right to left", func(t *testing.T) {
+		initial := 2
+		expected := 6
+
+		if fp.Compose2(double, add1)(initial) != expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("composes across types", func(t *testing.T) {
+		initial := "hello"
+		expected := 10
+
+		if fp.Compose2(double, strlen)(initial) != expected {
+			t.Fail()
+		}
+	})
+}
+
+func TestCompose3(t *testing.T) {
+	if fp.Compose3(add1, double, double)(2) != 9 {
+		t.Fatal("should compose 3 functions right to left")
+	}
+}
+
+func TestCompose4(t *testing.T) {
+	if fp.Compose4(add1, add1, double, double)(2) != 10 {
+		t.Fatal("should compose 4 functions right to left")
+	}
+}
+
+func TestNested(t *testing.T) {
+	add1 := func(x int) int { return x + 1 }
+	double := func(x int) int { return x * 2 }
+	triple := func(x int) int { return x * 3 }
+
+	result4 := fp.Compose4(add1, double, add1, triple)(5) // performs triple -> add1 -> double -> add1
+	result2 := fp.Compose2(fp.Compose2(add1, double), fp.Compose2(add1, triple))(5)
+
+	if result2 != result4 {
+		t.Fatal("nesting compose should be equal to larger compose")
+	}
+}
+
+func TestCompose5(t *testing.T) {
+	if fp.Compose5(add1, add1, add1, double, double)(2) != 11 {
+		t.Fatal("should compose 5 functions right to left")
+	}
+}
+
+func TestCompose6(t *testing.T) {
+	if fp.Compose6(add1, add1, add1, add1, double, double)(2) != 12 {
+		t.Fatal("should compose 6 functions right to left")
+	}
+}
+
+func TestCompose7(t *testing.T) {
+	if fp.Compose7(add1, add1, add1, add1, add1, double, double)(2) != 13 {
+		t.Fatal("should compose 7 functions right to left")
+	}
+}
+
+func TestCompose8(t *testing.T) {
+	if fp.Compose8(add1, add1, add1, add1, add1, add1, double, double)(2) != 14 {
+		t.Fatal("should compose 8 functions right to left")
+	}
+}
+
+func TestCompose9(t *testing.T) {
+	if fp.Compose9(add1, add1, add1, add1, add1, add1, add1, double, double)(2) != 15 {
+		t.Fatal("should compose 9 functions right to left")
+	}
+}
