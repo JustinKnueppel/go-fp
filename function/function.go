8b3ad604e1bddc5b2a8f5507aabff3600289ce87
Justin Knueppel <32350916+JustinKnueppel@users.noreply.github.com>
2022-09-11 23:28:32 -0400

    Add increment and decrement (#15)
    * Add increment and decrement
    
    * Update documentation

--- function/function.go
+++ function/function.go
@@ -1,5 +1,15 @@
 package function
 
+// Decrement returns the given number decremented.
+func Decrement(x int) int {
+	return x - 1
+}
+
+// Increment returns the given number incremented.
+func Increment(x int) int {
+	return x + 1
+}
+
 // Inspect calls the given function with the provided value
 // and returns the unchanged value.
 func Inspect[T any](fn func(T)) func(T) T {
