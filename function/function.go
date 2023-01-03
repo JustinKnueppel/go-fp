5ac0635bc973a3367ff263b1039f0d2f512cb2d5
Justin Knueppel <justinknueppel@gmail.com>
2023-01-03 03:23:42 -0500

    Add on function

--- function/function.go
+++ function/function.go
@@ -8,3 +8,15 @@ func Inspect[T any](fn func(T)) func(T) T {
 		return t
 	}
 }
+
+// On transforms a binary operation on type B into a binary operation on type A
+// by applying a transformation from A to B before operating.
+func On[A, B, C any](fn func(B) func(B) C) func(func(A) B) func(A) func(A) C {
+	return func(transform func(A) B) func(A) func(A) C {
+		return func(a1 A) func(A) C {
+			return func(a2 A) C {
+				return fn(transform(a1))(transform(a2))
+			}
+		}
+	}
+}
--- function/function_test.go
+++ function/function_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	fp "github.com/JustinKnueppel/go-fp/function"
+	"github.com/JustinKnueppel/go-fp/operator"
+	"github.com/JustinKnueppel/go-fp/slice"
+	"github.com/JustinKnueppel/go-fp/tuple"
 )
 
 func ExampleInspect() {
@@ -17,4 +20,24 @@ func ExampleInspect() {
 
 	// Output:
 	// X has value: 6
+}
+
+func ExampleOn() {
+	length := func(s string) int { return len(s) }
+	newPair := func(x int, s string) tuple.Pair[int, string] { return tuple.NewPair[int, string](x)(s) }
+
+	fp.Pipe2(
+		// Give the ability to sort these tuples based on the length of the string
+		slice.Sort(fp.On[tuple.Pair[int, string]](operator.Lt[int])(fp.Compose2(length, tuple.Snd[int, string]))),
+		fp.Inspect(func(pairs []tuple.Pair[int, string]) {
+			fmt.Println(slice.Map(pairToString[int, string])(pairs))
+		}),
+	)([]tuple.Pair[int, string]{newPair(3, "h"), newPair(2, "foo"), newPair(4, "ts")})
+
+	// Output:
+	// [(3 h) (4 ts) (2 foo)]
+}
+
+func pairToString[T, U any](pair tuple.Pair[T, U]) string {
+	return fmt.Sprintf("(%v %v)", tuple.Fst(pair), tuple.Snd(pair))
 }
